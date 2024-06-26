5a619b1e651f9a0ed63b5d3b9573ab8c1cc8f9f2
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-06-26 13:33:48 +0200

    0:54:00

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+//const amount = 12;
+const amount = 9;
+
+if(amount < 10){
+
+    console.log('numero pequeño');
+
+}else{
+    console.log('numero grande');
+}
+
+console.log(`hey it's my first node app, hello world`)
---
+++ 5-utils.js
@@ -0,0 +1,7 @@
+//second module
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+// Esto es muy similar el export as default
+module.exports = sayHi

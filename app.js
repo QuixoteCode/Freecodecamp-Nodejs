a0d19adb0190c8d88dadc79148e9ec87cd771a08
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-07-05 19:24:35 +0200

    3:12:50

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const { readFile, writeFile } = require('fs')
-const util = require('util');
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile)
+const { readFile, writeFile } = require('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 const start = async() =>{
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf8')
-        const second = await readFilePromise('./content/second.txt', 'utf8')
-        await writeFilePromise('./content/result-mind-grenade.txt', `ESTO ES INCREIBLE : ${first} ${second}`)
+        const first = await readFile('./content/first.txt', 'utf8')
+        const second = await readFile('./content/second.txt', 'utf8')
+        await writeFile(
+            './content/result-mind-grenade.txt',
+            `ESTO ES INCREIBLE : ${first} ${second}`,
+            {flag:'a'})
         console.log(first, second)
     } catch(error) {
         console.log(error)
